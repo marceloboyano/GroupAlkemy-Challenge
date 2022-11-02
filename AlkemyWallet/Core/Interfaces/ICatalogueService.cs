e1c74be23660a8473cadab66ae1d2ea4379c22fb
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-11-02 15:38:41 -0300

    Agrego el controlador de account y catalogue , asi como los servicios e interfaces de ambos. inyecto los servicios y repos en program.

---
+++ AlkemyWallet/Core/Interfaces/ICatalogueService.cs
@@ -0,0 +1,11 @@
+﻿using AlkemyWallet.Core.Models;
+using AlkemyWallet.Core.Models.DTO;
+
+namespace AlkemyWallet.Core.Interfaces
+{
+    public interface ICatalogueService
+    {
+        Task<IEnumerable<Catalogue>> GetCatalogues();
+        Task<Catalogue> GetCatalogueById(int id);
+    }
+}
--- AlkemyWallet/Program.cs
+++ AlkemyWallet/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
 using AlkemyWallet.Core.Models;
+using AlkemyWallet.Repositories.Interfaces;
+using AlkemyWallet.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,13 +15,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IUserService, UserService>();
+
 builder.Services.AddDbContext<WalletDbContext>
     (options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseContext")); });
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                   .AddEntityFrameworkStores<WalletDbContext>()
                   .AddDefaultTokenProviders();
+
+// Agrego los repositorios
+builder.Services.AddScoped<ICatalogueRepository, CatalogueRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<IFixedTermDepositRepository, FixedTermDepositRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Agrego los servicios
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICatalogueService, CatalogueService>();
+builder.Services.AddScoped<IAccountsService, AccountsService>();
+
+//MAPPER
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
