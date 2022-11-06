c8c85af86c4c0b8fe0a2058ad2c5e62f82fb56c5
jasonfabian <ye_prog@hotmail.com>
2022-11-06 17:13:11 -0500

    Transaction Controller 35. Endpoint list transactions 36. Endpoint detail transaction Validating user authentication

--- AlkemyWallet/Controllers/TransactionsController.cs
+++ AlkemyWallet/Controllers/TransactionsController.cs
@@ -17,30 +17,35 @@ namespace AlkemyWallet.Controllers
         
         private readonly ITransactionService _transactionService;
         private readonly IMapper _mapper;
+        private readonly IUserService _userService;
 
-        public TransactionsController(ITransactionService transactionService, IMapper mapper)
+        public TransactionsController(ITransactionService transactionService, IMapper mapper, IUserService userService)
         {
             _transactionService = transactionService;
             _mapper = mapper;
+            _userService = userService;
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetTransactions()
         {
-            int userId = 2;
+            int userId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(x => x.Type.ToString().Equals("uid"))!.Value);
             var transactions = await _transactionService.GetTransactions(userId);
             var transactionsForShow = _mapper.Map<IEnumerable<TransactionDTO>>(transactions);
             return Ok(transactionsForShow);
         }
 
+
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<IActionResult> GetTransactionsById(int id)
         {
-            int userId = 2;
-            var trans = await _transactionService.GetTransactionsById(id, userId);
+            int userId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(x => x.Type.ToString().Equals("uid"))!.Value);
+            var trans = await _transactionService.GetTransactionById(id, userId);
             if (trans is null)
             {
-                return NotFound("No existe ningún catalogo con el id especificado");
+                return NotFound("No existe ningun catalogo con el id especificado");
             }
             var transForShow = _mapper.Map<TransactionDTO>(trans);
             return Ok(transForShow);
