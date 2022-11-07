cc3c932d87ce2e95655165281e950ffb7dedc523
jasonfabian <ye_prog@hotmail.com>
2022-11-07 09:40:46 -0500

    Fix conflicts merge

--- AlkemyWallet/Core/Interfaces/ITransactionService.cs
+++ AlkemyWallet/Core/Interfaces/ITransactionService.cs
@@ -1,13 +1,12 @@
 using AlkemyWallet.Entities;
 
-namespace AlkemyWallet.Core.Interfaces
+namespace AlkemyWallet.Core.Interfaces;
+
+public interface ITransactionService
 {
-    public interface ITransactionService
-    {
-        Task<bool> DeleteTransaction(int id);
-        Task InsertTransaction(Transaction transaction);
-        Task<bool> UpdateTransaction(int id, Transaction transaction);
-        Task<IEnumerable<Transaction>> GetTransactions(int userId);
-        Task<Transaction> GetTransactionById(int id, int userId);
-    }
-} 
+    Task<bool> DeleteTransaction(int id);
+    Task InsertTransaction(Transaction transaction);
+    Task<bool> UpdateTransaction(int id, Transaction transaction);
+    Task<IEnumerable<Transaction>> GetTransactions(int userId);
+    Task<Transaction> GetTransactionById(int id, int userId);
+}
