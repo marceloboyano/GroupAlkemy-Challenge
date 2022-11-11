6c836270b5717fef7e78afd2dcf9b86a9cd4c332
hipogrifo010 <46372474+hipogrifo010@users.noreply.github.com>
2022-11-10 21:39:00 -0300

    Seed-update
    division de los seed para mejor manejo

---
+++ AlkemyWallet/DataAccess/Seeds/CatalogueSeeder.cs
@@ -0,0 +1,97 @@
+﻿using AlkemyWallet.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlkemyWallet.DataAccess.Seeds
+{
+    public class CatalogueSeeder
+    {
+        //falta el interface para cargar todo a dbcontext de una 
+        private readonly ModelBuilder _modelBuilder;
+
+        public CatalogueSeeder(ModelBuilder modelBuilder)
+        {
+            _modelBuilder = modelBuilder;
+        }
+
+        public void SeedCatalogue()
+        {
+
+            _modelBuilder.Entity<Catalogue>(p =>
+            {
+                p.HasData(
+                 new Catalogue
+                 {
+                     Id = 1,
+                     Product_description = "cocina",
+                     Image = "",
+                     Points = 300
+                 },
+                 new Catalogue
+                 {
+                     Id = 2,
+                     Product_description = "Lavarropas",
+                     Image = "",
+                     Points = 500
+                 },
+                 new Catalogue
+                 {
+                     Id = 3,
+                     Product_description = "Heladera",
+                     Image = "",
+                     Points = 700
+                 },
+                 new Catalogue
+                 {
+                     Id = 4,
+                     Product_description = "Lavavajillas",
+                     Image = "",
+                     Points = 400
+                 },
+                 new Catalogue
+                 {
+                     Id = 5,
+                     Product_description = "Freezer",
+                     Image = "",
+                     Points = 600
+                 },
+                 new Catalogue
+                 {
+                     Id = 6,
+                     Product_description = "Microondas",
+                     Image = "",
+                     Points = 200
+                 },
+                 new Catalogue
+                 {
+                     Id = 7,
+                     Product_description = "Horno Electrico",
+                     Image = "",
+                     Points = 400
+                 },
+                 new Catalogue
+                 {
+                     Id = 8,
+                     Product_description = "Horno Grande",
+                     Image = "",
+                     Points = 500
+                 },
+                 new Catalogue
+                 {
+                     Id = 9,
+                     Product_description = "Panificadora",
+                     Image = "",
+                     Points = 200
+                 },
+                 new Catalogue
+                 {
+                     Id = 10,
+                     Product_description = "Cepillo Electrico",
+                     Image = "",
+                     Points = 100
+                 }
+             );
+            });
+        }
+    }
+
+}
---
+++ AlkemyWallet/DataAccess/Seeds/TransactionSeeder.cs
@@ -0,0 +1,95 @@
+﻿using AlkemyWallet.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlkemyWallet.DataAccess.Seeds
+{
+    public class TransactionSeeder
+    {
+        private readonly ModelBuilder _modelBuilder;
+
+        public TransactionSeeder(ModelBuilder modelBuilder)
+        {
+            _modelBuilder = modelBuilder;
+        }
+
+        public void SeedTransaction()
+        {
+            _modelBuilder.Entity<Transaction>(p =>
+            {
+                p.HasData(
+                new Transaction
+                {
+                    Transaction_id = 1,
+                    Amount = 15155,
+                    Concept = "crédito",
+                    Date = new DateTime(2020, 01, 25),
+                    Type = "Efectivo",
+                    Account_id = 1,
+                    User_id = 2
+                },
+                new Transaction
+                {
+                    Transaction_id = 2,
+                    Amount = 4922,
+                    Concept = "Efectivo",
+                    Date = new DateTime(1999, 04, 14),
+                    Type = "Debito",
+                    Account_id = 2,
+                    User_id = 2
+                },
+                new Transaction
+                {
+                    Transaction_id = 3,
+                    Amount = 9340,
+                    Concept = "Prestamo",
+                    Date = new DateTime(2002, 03, 04),
+                    Type = "crédito",
+                    Account_id = 1,
+                    User_id = 1
+                },
+                new Transaction
+                {
+                    Transaction_id = 4,
+                    Amount = 3211,
+                    Concept = "Sueldo",
+                    Date = new DateTime(2013, 05, 25),
+                    Type = "Efectivo",
+                    Account_id = 1,
+                    User_id = 1
+                },
+                new Transaction
+                {
+                    Transaction_id = 5,
+                    Amount = 55552,
+                    Concept = "crédito",
+                    Date = new DateTime(2022, 02, 18),
+                    Type = "Efectivo",
+                    Account_id = 2,
+                    User_id = 2
+                },
+                new Transaction
+                {
+                    Transaction_id = 6,
+                    Amount = 224,
+                    Concept = "Reintegro",
+                    Date = new DateTime(1980, 08, 12),
+                    Type = "Debito",
+                    Account_id = 1,
+                    User_id = 2
+                },
+                new Transaction
+                {
+                    Transaction_id = 7,
+                    Amount = 202300,
+                    Concept = "Pago",
+                    Date = new DateTime(1990, 11, 22),
+                    Type = "Efectivo",
+                    Account_id = 1,
+                    User_id = 1
+                }
+            );
+            });
+        }
+    }
+
+}
