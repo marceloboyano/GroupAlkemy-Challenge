ade47ea0ebf9187d10a02dba9f5584110434871a
jasonfabian <ye_prog@hotmail.com>
2022-11-10 22:24:56 -0500

    Fixed conflict with interface

--- AlkemyWallet/Core/Interfaces/ICatalogueService.cs
+++ AlkemyWallet/Core/Interfaces/ICatalogueService.cs
@@ -10,8 +10,8 @@ public interface ICatalogueService
     Task<bool> DeleteCatalogue(int id);
     Task<IEnumerable<Catalogue>> GetCatalogues();
     Task<Catalogue?> GetCatalogueById(int id);
-    Task<IEnumerable<Catalogue>> GetCatalogueByPoints(int points);
+    Task<IEnumerable<Catalogue>> GetCatalogueByPoints(int userId);
     Task InsertCatalogue(CatalogueForCreationDTO catalogue);
-    Task<bool> UpdateCatalogues(int id, CatalogueForUpdateDTO CatalogueDTO);
+    Task<bool> UpdateCatalogues(int id, CatalogueForUpdateDTO catalogueDTO);
     Task<(int totalPages, IEnumerable<Catalogue> recordList)> GetCataloguesPaging(int pageNumber, int pageSize);
 }
